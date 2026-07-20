cfcf006971b0cfe305e111d538b6ea6024a12555
hackerbhai544 <hackerbhai544@gmail.com>
2026-07-20 09:42:00 +0530

    Add files via upload

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -75,7 +75,7 @@ exports.verifyPayment = async (req, res) => {
         const body = razorpay_order_id + "|" + razorpay_payment_id;
 
         const expectedSignature = crypto
-            .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+            .createHmac("sha256", process.env.RAZORPAY_SECRET)
             .update(body)
             .digest("hex");
 
